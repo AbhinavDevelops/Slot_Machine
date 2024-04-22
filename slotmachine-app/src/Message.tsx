8e04ea30a189641242836879b1516b53937a1f93
Abhinav Rajaram <23082057@student.uwa.edu.au>
2024-04-22 10:24:17 +0800

    added react library and created the website, have to do routing next

---
+++ slotmachine-app/src/Message.tsx
@@ -0,0 +1,12 @@
+//PascalCasing
+function Message(){
+
+    return (
+        <div>
+            <h1>Slot Machine</h1>
+            <p>Press the button to start the game</p>
+        </div>
+    );
+}
+
+export default Message;
